23384205270aaa8b71a840f9188085bcc981277a
Jean Carlos <jean.silva0096@gmail.com>
2024-07-12 01:29:06 -0300

    Customizando respostas de erros customizadas para serem interceptadas pelos controllers

---
+++ src/main/java/com/tech/challenge/zonaAzul/condutor/controller/exception/ValidationError.java
@@ -0,0 +1,27 @@
+package com.tech.challenge.zonaAzul.condutor.controller.exception;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class ValidationError extends StandardError {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+        super(timestamp, status, error, message, path);
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+
+}
